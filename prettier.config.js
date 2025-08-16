31ba23609e93c7e8525bca52dc09f343c8eed097
Rxlling-Pxly <44181024+Rxlling-Pxly@users.noreply.github.com>
2025-08-16 09:50:42 -0700

    Cleaned up root, archive, and lib part 2

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,16 @@
 /**
  * If you have the Prettier VS Code extension installed it will use this config
+ * If you install it, make sure to set Prettier as the default formatter in VS Code settings
  * @see https://prettier.io/docs/configuration
  * @see https://prettier.io/docs/options
  * @see https://prettier-config-generator.com
- * @type {import("prettier").Config}
  */
 const config = {
   arrowParens: "always",
   bracketSameLine: false,
   bracketSpacing: true,
   embeddedLanguageFormatting: "auto",
-  endOfLine: "crlf",
+  endOfLine: "lf",
   experimentalOperatorPosition: "end",
   experimentalTernaries: true,
   htmlWhitespaceSensitivity: "css",
@@ -29,12 +29,10 @@ const config = {
   useTabs: false,
 };
 
-
-// Though ESLint requires Node, we want to be able to use Prettier without needing Node
-// in case someone ever wanted to use Prettier but not ESLint
-
-// And to do that, we must write this config file in CommonJS,
-// which ESLint was not configured to understand for this project
-
+// Though ESLint requires Node, we want to be able to use Prettier without needing it as well
+// This is case someone ever wanted to use Prettier but not ESLint
+// To do that, we must write this config file in CommonJS,
+// However, ESLint was not configured to understand that for this project
+// So we need the following line:
 // eslint-disable-next-line no-undef
 module.exports = config;
